64ac8b8ac711168ca23a2f0c38b73bf563dcb1e3
Tirthajyoti Ghosh <itirthahere@gmail.com>
2022-01-09 19:17:36 +0530

    Add reset and container styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import EmailBody from './component/EmailBody';
 import EmailList from './component/EmailList';
 import Filter from './component/Filter';
 
+import './App.css';
+
 const App = () => {
     const [emailId, setEmailId] = useState();
 
     return (
-        <>
+        <div className="container">
             <Filter />
             <main className="App">
                 <EmailList setEmailId={setEmailId} />
                 <EmailBody emailId={emailId} />
             </main>
-        </>
+        </div>
     );
 };
 
